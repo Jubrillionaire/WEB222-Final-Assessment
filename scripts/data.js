bff0f96507acf1395f73f101f56902ca49fb8ae6
Jubrillionaire <jubrillionaire@gmail.com>
2023-08-14 17:32:37 -0400

    add data to home page

--- scripts/data.js
+++ scripts/data.js
@@ -1,32 +1,26 @@
 window.reviewData = [
   {
-    name: "John Doe",
-    date: "2023-05-15",
+    name: "John",
+    date: "2023-05-10",
     rating: 4,
-    review: "A hidden gem indeed! Loved the experience.",
+    review: "This is great.",
   },
   {
-    name: "Jane Smith",
+    name: "Joseph",
     date: "2023-05-14",
     rating: 5,
-    review: "Absolutely amazing place! Must visit.",
+    review: "Must visit!",
   },
   {
-    name: "Hermoine Grinder",
+    name: "Sully Robert",
     date: "2023-05-12",
     rating: 5,
-    review: "Absolutely amazing place!",
+    review: "Never seen comething like this before! Amazing",
   },
   {
-    name: "Will smith",
+    name: "Edwards",
     date: "2023-05-10",
     rating: 5,
-    review: "Absolutely sweet place! Must visit.",
-  },
-  {
-    name: "Micheal Scott",
-    date: "2023-05-20",
-    rating: 5,
-    review: "Absolutely amazing! Must visit.",
-  },
+    review: "Sweet!",
+  }
 ];
